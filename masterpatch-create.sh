35777f15f5d5374be8868e0e74c20c49711ef020
bluesnogbox <mybluesnogbox@gmail.com>
2015-02-23 16:10:28 -0500

    fixed some logic errors...derp

--- masterpatch-create.sh
+++ masterpatch-create.sh
@@ -12,8 +12,7 @@ system_lib_modules=( dhd.ko scsi_wait_scan.ko )
 system_lib_usr_keylayout=( gpio-keys.kl )
 
 if [ ! -d ~/android/dependencies ]; then
-  mkdir -p ~/android/dependencies
-  mv ./dependencies/ ~/android/dependencies
+  mv ./dependencies ~/android/
 fi
 
 if [ ! -d ~/bin ]; then
@@ -25,48 +24,48 @@ fi
 
 mkdir -p ~/android/masterpatch.${day}
 cd ~/android/masterpatch.${day}
-cur_path="~/android/masterpatch.${day}"
+patch_path="${HOME}/android/masterpatch.${day}"
 mkdir -p ./system/bin ./system/cameradata ./system/etc ./system/lib/hw ./system/lib/modules system/usr/keylayout
 
 adb start-server
 adb shell "su -c 'mount -o remount,rw /system'"
 
-cd ${cur_path}/system/bin
+cd ${patch_path}/system/bin
 for x in ${system_bin[@]}; do
   adb pull /system/bin/${x} ./
 done
 
-cd ${cur_path}/system/cameradata
+cd ${patch_path}/system/cameradata
 for x in ${system_cameradata[@]}; do
   adb pull /system/cameradata/${x} ./
 done
 
-cd ${cur_path}/system/etc
+cd ${patch_path}/system/etc
 for x in ${system_etc[@]}; do
   adb pull /system/etc/${x} ./
 done
 
-cd ${cur_path}/system/lib
+cd ${patch_path}/system/lib
 for x in ${system_lib[@]}; do
   adb pull /system/lib/${x} ./
 done
 
-cd ${cur_path}/system/lib/hw
+cd ${patch_path}/system/lib/hw
 for x in ${system_lib_hw[@]}; do
   adb pull /system/lib/hw/${x} ./
 done
 
-cd ${cur_path}/system/lib/modules
+cd ${patch_path}/system/lib/modules
 for x in ${system_lib_modules[@]}; do
   adb pull /system/lib/modules/${x} ./
 done
 
-cd ${cur_path}/system/usr/keylayout
+cd ${patch_path}/system/usr/keylayout
 for x in ${system_usr_keylayout[@]}; do
   adb pull /system/usr/keylayout/${x} ./
 done
 
-cd $cur_path
+cd $patch_path
 mkdir -p META-INF/com/google/android
 cp ../dependencies/* ./META-INF/com/google/android/
 zip -rv ./masterpatch.${day}.zip ./META-INF ./system
@@ -74,4 +73,4 @@ rm -rf ./META-INF ./system
 
 adb shell "su -c 'mount -o remount,ro /system'"
 adb kill-server
-echo -e "Master patch is done. It is located at ${cur_path}/masterpatch.${day}.zip"
+echo -e "Master patch is done. It is located at ${patch_path}/masterpatch.${day}.zip"
