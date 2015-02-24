75d46df6694e78f54ed72f5e4f032fdb3c3f0ba7
bluesnogbox <mybluesnogbox@gmail.com>
2015-02-23 19:30:52 -0500

    fixed more derps >.<

--- masterpatch-create.sh
+++ masterpatch-create.sh
@@ -9,7 +9,7 @@ system_etc=( media_profiles.xml )
 system_lib=( libchromatix_imx074_default_video.so libchromatix_imx074_preview.so libchromatix_imx074_video_hd.so libchromatix_imx074_zsl.so libchromatix_imx091_default_video.so libchromatix_imx091_preview.so libchromatix_imx091_video_hd.so libchromatix_mt9e013_default_video.so libchromatix_mt9e013_preview.so libchromatix_mt9e013_video_hfr.so libchromatix_ov2720_default_video.so libchromatix_ov2720_hfr.so libchromatix_ov2720_preview.so libchromatix_ov2720_zsl.so libchromatix_ov5647_default_video.so libchromatix_ov5647_preview.so libchromatix_ov8825_default_video.so libchromatix_ov8825_preview.so libchromatix_ov9726_default_video.so libchromatix_ov9726_preview.so libchromatix_s5k3h5xa_animation.so libchromatix_s5k3h5xa_antishake.so libchromatix_s5k3h5xa_default_video.so libchromatix_s5k3h5xa_lls.so libchromatix_s5k3h5xa_preview.so libchromatix_s5k3h5xa_zsl_drama.so libchromatix_s5k3h5xa_zsl_panorama.so libchromatix_s5k3h5xa_zsl.so libchromatix_s5k3l1yx_default_video.so libchromatix_s5k3l1yx_hfr_120fps.so libchromatix_s5k3l1yx_hfr_60fps.so libchromatix_s5k3l1yx_hfr_90fps.so libchromatix_s5k3l1yx_preview.so libchromatix_s5k3l1yx_video_hd.so libchromatix_s5k3l1yx_zsl.so libchromatix_s5k4e1_default_video.so libchromatix_s5k4e1_preview.so libchromatix_s5k6b2yx_pip.so libchromatix_s5k6b2yx_preview.so libchromatix_s5k6b2yx_smartstay.so libchromatix_s5k6b2yx_video.so libchromatix_s5k6b2yx_vt_hd.so libchromatix_s5k6b2yx_vt.so libgemini.so libimage-jpeg-enc-omx-comp.so libimage-omx-common.so libmercury.so libmmcamera_faceproc.so libmmcamera_frameproc.so libmmcamera_hdr_lib.so libmmcamera_image_stab.so libmmcamera_imx091.so libmmcamera_interface2.so libmmcamera_interface.so libmmcamera_plugin.so libmmcamera_statsproc31.so libmmcamera_wavelet_lib.so libmmjpeg_interface.so libmmjpeg.so libmmmpod.so libmmmpo.so libmmstillomx.so liboemcamera.so libsecnativefeature.so libTsAccm.so libTsAwb.so )
 system_lib_hw=( camera.msm8960.so )
 system_lib_modules=( dhd.ko scsi_wait_scan.ko )
-system_lib_usr_keylayout=( gpio-keys.kl )
+system_usr_keylayout=( gpio-keys.kl )
 
 adb start-server
 adb shell "su -c 'mount -o remount,rw /system'"
@@ -18,7 +18,7 @@ sleep 5
 mkdir -p ~/android/masterpatch
 cp ~/masterpatch-create/update-binary ~/android/masterpatch/
 cur_path=${HOME}/android/masterpatch
-mkdir -p ~/android/masterpatch/system/lib/usr/keylayout ~/android/masterpatch/system/lib/modules ~/android/masterpatch/system/lib/hw ~/android/masterpatch/system/bin ~/android/masterpatch/system/etc ~/android/masterpatch/system/cameradata
+mkdir -p ~/android/masterpatch/system/usr/keylayout ~/android/masterpatch/system/lib/modules ~/android/masterpatch/system/lib/hw ~/android/masterpatch/system/bin ~/android/masterpatch/system/etc ~/android/masterpatch/system/cameradata
 
 cd ${cur_path}/system/bin
 for x in ${system_bin[@]}; do
@@ -50,7 +50,7 @@ for x in ${system_lib_modules[@]}; do
   adb pull /system/lib/modules/${x} ./
 done
 
-cd ${cur_path}/system/lib/usr/keylayout
+cd ${cur_path}/system/usr/keylayout
 for x in ${system_usr_keylayout[@]}; do
   adb pull /system/usr/keylayout/${x} ./
 done
